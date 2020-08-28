92af15976cbf1d9a9927c0ff08c309cddde662a7
jubakitiashvili <juba.kitiashvili@gmail.com>
2020-08-28 14:23:02 +0400

    pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+        totam? Aliquam quae animi placeat, repellat sunt nobis consectetur iure
+        minima assumenda eum! Enim vitae autem voluptatem quam in numquam
+        aliquid explicabo temporibus, nisi nostrum rerum vero eum doloribus
+        provident, mollitia cupiditate architecto dicta sequi itaque totam error
+        voluptas minus! Commodi aperiam similique, aspernatur quis aliquid
+        numquam incidunt eos non adipisci!
+      </p>
+    </section>
+  );
 }
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
-  return <h1>error page</h1>;
+  return (
+    <section className="section error-section">
+      <div className="error-container">
+        <h1>oops! it's a dead link</h1>
+        <Link to="/" className="btn btn-primary">
+          return to home page
+        </Link>
+      </div>
+    </section>
+  );
 }
